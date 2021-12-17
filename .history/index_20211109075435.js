d58e221ff05a8034a6f1456f5a95080d1b3b8bb6
thuong22062004 <nvt137609@gmail.com>
2021-12-17 22:48:32 +0700

    capnhatcode

--- .history/index_20211109075435.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load',function(){
-  const slider = document.querySelector('.slider');
-  const mainslider = document.querySelector('contain-img');
-  const next = document.querySelector('.slider__next');
-  const back = document.querySelector('.slider__back');
-  const itemdots = d
-})
